2c75acd44e92d4be77c7ddc6af7e894922b34096
Tata Alyssa <imhelium02@gmail.com>
2025-07-04 22:53:43 +0700

    b

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # bisa juga pakai ["*"] untuk semua origin (tidak disarankan untuk production)
+    allow_origins=["*"],  # bisa juga pakai ["*"] untuk semua origin (tidak disarankan untuk production)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
